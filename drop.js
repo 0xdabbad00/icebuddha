9dd3dad399e602188d3fa7a9bbdb668db5f24ce3
0x1ceb00da@gmail.com <0x1ceb00da@gmail.com@9149f89a-7433-b8a5-3c47-2eb31880b585>
2012-03-30 01:08:15 +0000

    Hex output split into 3 columns (address, hex, and ascii)

--- drop.js
+++ drop.js
@@ -7,7 +7,7 @@
 // TODO: Add progress reporting and error handling from http://www.html5rocks.com/en/tutorials/file/dndfiles/#toc-monitoring-progress
 
 
-function hex(dec)
+function convertToHex(dec)
 {
     var hexArray = new Array( "0", "1", "2", "3",
                               "4", "5", "6", "7",
@@ -40,50 +40,63 @@ function handleFinishedRead(evt) {
 		var output = ["<div id='byte_content'>"];
 		var data =  new Uint8Array(evt.target.result, 0, evt.target.result.byteLength);
 		
+		var address = [""];
+		var hex = [""];
+		var ascii = [""];
+		
 		var column = 0;
 		for (var i = 0; i < data.length; i++) {
 			// Show address
 			if (column == 0) {
-				output.push("<i class=address>0x"+intToHex(i)+"</i>&nbsp;&nbsp;");
+				address.push("<i class=address>0x"+intToHex(i)+"</i><br>\n");
 			}
 			// Show value
-			output.push(hex((data[i]&0xf0)>>4));
-			output.push(hex(data[i]&0x0f));
+			hex.push(convertToHex((data[i]&0xf0)>>4));
+			hex.push(convertToHex(data[i]&0x0f));
 			
 			// Add extra formatting
 			column++;
 			if (column % 16 == 0) {
-				output.push("&nbsp;&nbsp;<i class=ascii>");
+				hex.push("<br>\n");
+				ascii.push("<i class=ascii>");
 				// Show ASCII
 				for (j = i - 15; j <= i; j++) {
-					output.push(dispAscii(data[j]));
+					ascii.push(dispAscii(data[j]));
 				}
-				output.push("</i><br>\n");
+				ascii.push("</i><br>\n");
 				column = 0;
 			} else if (column % 8 == 0) {
-				output.push("&nbsp;");
+				hex.push("&nbsp;");
 			}
-			output.push(" ");
+			hex.push(" ");
 		}
 		
 		if (i % 16 != 0) {
 			// Pad to ascii
 			for (j = i%16; j < 16; j++) {
 				//output.push("&nbsp;&nbsp;&nbsp;");
-				output.push("&nbsp;&nbsp;&nbsp;");
+				hex.push("&nbsp;&nbsp;&nbsp;");
 				if (j % 8  == 0 && (i%16 != 8)) {
-					output.push("&nbsp;");
+					hex.push("&nbsp;");
 				}
 			}
 			
 			// Disp remaining ascii
-			output.push("&nbsp;<i class=ascii>");
+			ascii.push("&nbsp;<i class=ascii>");
 			for (j = i - (i%16); j <= i; j++) {
-				output.push(dispAscii(data[j]));
+				ascii.push(dispAscii(data[j]));
 			}
-			output.push("</i>");
+			ascii.push("</i>");
 		}
 		
+		output.push("<table border=0 cellpadding=0 cellspacing=0><tr><td style=\"padding: 0 10px 0 0;\">");
+		output.push(address.join(""));
+		output.push("<td style=\"padding: 0 10px 0 0;\">");
+		output.push(hex.join(""));
+		output.push("<td>");
+		output.push(ascii.join(""));
+		
+		output.push("</table>");
 		output.push("</div>");
 		document.getElementById('content').innerHTML = output.join("");
 		
