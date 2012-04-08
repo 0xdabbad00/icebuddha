5839e9bc4fd49c1656746145116e7c1f5b4e5d0e
0xdabbad00 <0xdabbad00@gmail.com>
2012-04-08 04:49:01 +0000

    Cleaned up code; added select on parseTree; cleaned up creation of parseTree; added data to parseTree

--- drop.js
+++ drop.js
@@ -1,5 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+// Globals
+///////////////////////////////////////////////////////////////////////////////
 var data;
+var file;
+var reader;
 
+
+///////////////////////////////////////////////////////////////////////////////
+// Utility functions
+///////////////////////////////////////////////////////////////////////////////
 function convertToHex(dec)
 {
     var hexArray = new Array( "0", "1", "2", "3",
@@ -10,12 +19,16 @@ function convertToHex(dec)
     return (decToHex);
 }
 
+function addHexIdentifier(value) {
+	return value+"h";
+}
+
 function intToHex(val) {
 	// Convert value to hex
 	var str = ''+val.toString(16);
 	// Pad with 0's
 	while(str.length < 8) str = '0'+str;
-	return str;
+	return addHexIdentifier(str);
 }
 
 function dispAscii(val) {
@@ -25,10 +38,12 @@ function dispAscii(val) {
 }
 
 
-
+///////////////////////////////////////////////////////////////////////////////
+// File reading
+///////////////////////////////////////////////////////////////////////////////
 function handleFinishedRead(evt) {
 	
-	if(evt.target.readyState == FileReader.DONE) {// DONE == 2
+	if(evt.target.readyState == FileReader.DONE) {
 		
 		var output = [""];
 		data =  new Uint8Array(evt.target.result, 0, evt.target.result.byteLength);
@@ -41,7 +56,7 @@ function handleFinishedRead(evt) {
 		for (var i = 0; i < data.length; i++) {
 			// Show address
 			if (column == 0) {
-				address.push(intToHex(i)+"h<br>\n");
+				address.push(intToHex(i)+"<br>\n");
 			}
 			// Show value
 			hex.push("<i id=\"h"+i+"\" class=\"hex\">");
@@ -93,7 +108,7 @@ function handleFileSelect(evt) {
 	var files = evt.dataTransfer.files; // FileList
 
 	var output = [];
-	var file = files[0];  // File object
+	file = files[0];  // File object
 	log.info("Loading: "+escape(file.name));
 	output.push('<strong>' + escape(file.name)+ '</strong> - ' + file.size + ' bytes');
 	document.getElementById('subheader').innerHTML = output.join(); 
@@ -107,12 +122,14 @@ function handleFileSelect(evt) {
 	output.push("<div id=\"parsetree\"></div>\n");
 	document.getElementById('content').innerHTML = output.join("");
 
-	var reader = new FileReader();
+	reader = new FileReader();
 	reader.onloadend = handleFinishedRead;
 	
+	readFileSlice(0, 16*100);
+}
+
+function readFileSlice(start, end) {
 	// Determine how much to read
-	var start = 0;
-	var end = 16*100;
 	if(file.webkitSlice) {
 		var blob = file.webkitSlice(start, end);
 	} else if(file.mozSlice) {
@@ -126,10 +143,12 @@ function handleDragOver(evt) {
 	evt.stopPropagation();
 	evt.preventDefault();
 	evt.dataTransfer.dropEffect = 'copy';
-	// Explicitly show this is a copy.
 }
 
-// Set mouse-over
+
+///////////////////////////////////////////////////////////////////////////////
+// Mouse hovering
+///////////////////////////////////////////////////////////////////////////////
 function mouseoverBytes() {
 	var currentId = this.id;
 	var byte = currentId.substring(1, currentId.length); 
@@ -150,13 +169,15 @@ function SetValueElement(offset) {
   var output = [""];
   var offsetInt = parseInt(offset);
   if (isNaN(offsetInt)) return;
-  output.push("Offset "+intToHex(offsetInt)+"h<br>");
+  output.push("Offset "+intToHex(offsetInt)+"<br>");
   output.push("Data &nbsp;&nbsp;"+
+		  addHexIdentifier(
 		  convertToHex(data[offsetInt])+
 		  convertToHex(data[offsetInt+1])+
 		  convertToHex(data[offsetInt+2])+
-		  convertToHex(data[offsetInt+3])+
-		  "h<br>");
+		  convertToHex(data[offsetInt+3])
+		  )+
+		  "<br>");
   output.push("ubyte&nbsp;&nbsp;"+
 		  data[offsetInt]+
 		  "<br>");
@@ -173,19 +194,44 @@ function SetValueElement(offset) {
   document.getElementById('value').innerHTML = output.join("");  
 }
 
+
+///////////////////////////////////////////////////////////////////////////////
+// Parse tree
+///////////////////////////////////////////////////////////////////////////////
+
+var expectedOffset = 0;
+function node(label, size, offset) {
+	offset = offset || expectedOffset;
+	expectedOffset = offset + size;
+	
+	var dataValue = "";
+	var maxDataDisplaySize = 4; 
+	for(var i=0; i<size && i<maxDataDisplaySize; i++) {
+		dataValue += convertToHex(data[offset+i]); 
+	}
+	if(size>maxDataDisplaySize) {
+		dataValue +="...";
+	}
+	dataValue = addHexIdentifier(dataValue);
+	
+	return {label: label, offset: offset, size: size, data: dataValue};
+}
+
+
 function SetParseTree() {
 	var treedata = [
 		            {
-		                label: 'IMAGE_DOS_HEADER', offset: 0, size: 40, format: 'hex',
+		                label: 'IMAGE_DOS_HEADER', offset: 0, size: 40, 
 		                children: [
-				                    { label: 'Signature: MZ', offset: 0, size: 2, format: 'ascii'},
-				                    { label: 'UsedBytesInTheLastPage: 140', offset: 2, size: 2, format: 'uint' },
-				                    { label: 'NumberOfRelocationItems: 3', offset: 4, size: 2, format: 'uint'  },
-				                    { label: 'HeaderSizeInParagraphs', offset: 6, size: 2, format: 'uint'  },
-				                    { label: 'MinimumExtraParagraphs', offset: 8, size: 2, format: 'uint'  },
-				                    { label: 'MaximumExtraParagraphs', offset: 10, size: 2, format: 'uint'  },
-				                    { label: 'InitialRelativeSS', offset: 12, size: 2, format: 'uint'  },
-				                    { label: 'InitialSP', offset: 14, size: 2, format: 'uint'  },
+				                    /*{ label: 'Signature: MZ', offset: 0, size: 2, data: data[0], format: 'ascii'},*/
+				                    node("Signature", 2, 0),
+				                    node("UsedBytesInTheLastPage", 2),
+				                    node("NumberOfRelocationItems", 8),
+				                    node("HeaderSizeInParagraphs", 2),
+				                    { label: 'MinimumExtraParagraphs', offset: 8, size: 2},
+				                    { label: 'MaximumExtraParagraphs', offset: 10, size: 2},
+				                    { label: 'InitialRelativeSS', offset: 12, size: 2 },
+				                    { label: 'InitialSP', offset: 14, size: 2},
 				                    { label: 'Checksum' },
 				                    { label: 'InitialIP' },
 				                    { label: 'InitialRelativeCS' },
@@ -220,27 +266,49 @@ function SetParseTree() {
 			    'tree.click',
 			    function(event) {
 			        var node = event.node;
+			        
+			        // High-lite byte data
+			        // Unset old
 			        for(var i=selectStart; i<selectEnd; i++) {
 				      $("#a"+i).removeClass( "selected");
 				      $("#h"+i).removeClass( "selected");
 				    }
 			        
+			        // Set new
 			        selectStart = node.offset;
 			        selectEnd = node.offset + node.size;
 			        for(var i=selectStart; i<selectEnd; i++) {
 			          $("#a"+i).addClass( "selected");
 			          $("#h"+i).addClass( "selected");
 			        }
+			        
+			        // High-lite parse tree
+			        // Unset old
+			        if (selectedNode != null) {
+			          selectedNode.removeClass("selected");
+			        }
+			        // Set new
+			        selectedNode = event.target;
+			        if (selectedNode.hasClass("parseTreeData")) {
+			        	selectedNode = selectedNode.parent();
+			        }
+			        selectedNode.addClass("selected");
+			         
+			        
 			    }
 			);
 }
 
+
+///////////////////////////////////////////////////////////////////////////////
+// Main
+///////////////////////////////////////////////////////////////////////////////
 var selectStart = 0;
 var selectEnd = 0;
+var selectedNode = null;
 
 //Setup the dnd listeners.
 var dropZone = document.getElementById('container');
 dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', handleFileSelect, false);
 
-SetParseTree();
