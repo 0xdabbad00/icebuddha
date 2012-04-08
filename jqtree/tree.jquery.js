b1ce1cf18ba56bba72d8a4d9b91303bd23b3cdb5
0x1ceb00da@gmail.com <0x1ceb00da@gmail.com@9149f89a-7433-b8a5-3c47-2eb31880b585>
2012-04-08 04:49:01 +0000

    Cleaned up code; added select on parseTree; cleaned up creation of parseTree; added data to parseTree

--- jqtree/tree.jquery.js
+++ jqtree/tree.jquery.js
@@ -553,7 +553,12 @@ limitations under the License.
         return $li;
       };
       createNodeLi = function(node) {
-        return $("<li><div><span class=\"title\">" + node.name + "</span></div></li>");
+        return $("<li><div><span class=\"title\">" + node.name + "<br>" +
+        		"<span class=\"parseTreeData\">" +
+        		"Offset: " + node.offset +
+        		"; Size: " + node.size +
+        		"; Data: " + node.data +
+        		"</span></span></div></li>");
       };
       createFolderLi = function(node) {
         var button_class, folder_class, getButtonClass, getFolderClass;
@@ -614,6 +619,7 @@ limitations under the License.
             this.selectNode(node);
             event = $.Event('tree.click');
             event.node = node;
+            event.target = $target;
             return this.element.trigger(event);
           }
         }
