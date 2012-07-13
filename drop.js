106270fa0862ad61772a8e996e0d90985ac27496
0x1ceb00da@gmail.com <0x1ceb00da@gmail.com@9149f89a-7433-b8a5-3c47-2eb31880b585>
2012-07-13 15:42:01 +0000

    Select entire hex word no matter what

--- drop.js
+++ drop.js
@@ -139,6 +139,7 @@ function doRead(readBlock, length) {
 	
 	$(".ascii").mouseover(mouseoverBytes).mouseout(mouseoutBytes);
 	$(".hex").mouseover(mouseoverBytes).mouseout(mouseoutBytes);
+	$(".hex").mouseup(snapSelectionToWord);
 	
 
 	if (!isValueElementSet) {
@@ -146,6 +147,54 @@ function doRead(readBlock, length) {
 	}
 	
 	SetParseTree();
+}
+
+function snapSelectionToWord() {
+	// Copied from http://jsfiddle.net/rrvw4/23/
+    var sel;
+
+    // Check for existence of window.getSelection() and that it has a
+    // modify() method. IE 9 has both selection APIs but no modify() method.
+    if (window.getSelection && (sel = window.getSelection()).modify) {
+        sel = window.getSelection();
+        if (!sel.isCollapsed) {
+
+            // Detect if selection is backwards
+            var range = document.createRange();
+            range.setStart(sel.anchorNode, sel.anchorOffset);
+            range.setEnd(sel.focusNode, sel.focusOffset);
+            var backwards = range.collapsed;
+            range.detach();
+
+            // modify() works on the focus of the selection
+            var endNode = sel.focusNode, endOffset = sel.focusOffset;
+            sel.collapse(sel.anchorNode, sel.anchorOffset);
+            
+            var direction = [];
+            if (backwards) {
+                direction = ['backward', 'forward'];
+            } else {
+                direction = ['forward', 'backward'];
+            }
+
+            sel.modify("move", direction[0], "character");
+            sel.modify("move", direction[1], "word");
+            sel.extend(endNode, endOffset);
+            sel.modify("extend", direction[1], "character");
+            sel.modify("extend", direction[0], "word");
+        }
+    } else if ( (sel = document.selection) && sel.type != "Control") {
+        var textRange = sel.createRange();
+        if (textRange.text) {
+            textRange.expand("word");
+            // Move the end back to not include the word's trailing space(s),
+            // if necessary
+            while (/\s$/.test(textRange.text)) {
+                textRange.moveEnd("character", -1);
+            }
+            textRange.select();
+        }
+    }
 }
 
 function getByteContentHTML(address, hex, ascii) {
