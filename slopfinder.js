853db1d00789dfcb190546b39566b0c5c408e46c
0xdabbad00 <0xdabbad00@gmail.com>
2012-11-07 20:41:44 +0100

    slopfinder added

---
+++ slopfinder.js
@@ -0,0 +1,121 @@
+///////////////////////////////////////////////////////////////////////////////
+// Globals
+///////////////////////////////////////////////////////////////////////////////
+var MAX_FILE_SIZE = 10*1024; // 10K
+var NUM_BYTES_TO_LOAD = 16*100;
+
+var fileData;
+var numFilesRead = 0;
+var filesToRead = 0;
+
+///////////////////////////////////////////////////////////////////////////////
+// File reading
+///////////////////////////////////////////////////////////////////////////////
+function handleFinishedRead(evt, i) {
+	if(evt.target.readyState == FileReader.DONE) {
+		var length = evt.target.result.byteLength;
+		var readBlock =  new Uint8Array(evt.target.result, 0, length);
+		doRead(readBlock, length, i);
+		numFilesRead++;
+		if (numFilesRead == filesToRead) {
+			displayResults();
+		}
+	}
+}
+
+function displayResults() {
+	var output = [""];
+	var executables = [""];
+	var unknownFiles = [""];
+	for (var i = 0; i < fileData.length; i++) {
+		file = fileData[i];
+		if (file.type == 'exe') {
+			executables.push("<tr><td>"+escape(file.name) + "<td align=center>"+file.dep+"<td align=center>"+file.aslr+"\n");
+		} else {
+			unknownFiles.push(escape(file.name) + "<br>\n");
+		}
+	}
+
+	if (executables.length>1) {
+		output.push("<b>Executables</b><br>"+
+			"<table border=0 cellpadding=0 cellspacing=0>"+
+			"<tr><th width=100>Name<th width=100>DEP<br>protection<th width=100>ALSR<br>protection</tr>" 
+			+ executables.join("")
+			+"</table>");
+	} else {
+		output.push("<b>No Windows executables found</b><br>");
+	}
+	if (unknownFiles.length>1) {
+		output.push("<br><b>Nonexecutables</b><br>" + unknownFiles.join(""));
+	}
+	$('#content').html(output.join(""));
+}
+
+
+function doRead(readBlock, length, i) {
+	
+	fileData[i].type = 'unknown';
+	// Check for MZ header
+	if (readBlock[0] == 'M'.charCodeAt(0) && readBlock[1] == 'Z'.charCodeAt(0))
+	{
+		fileData[i].type = 'exe';
+		fileData[i].dep = '<font color="red"><b>NO</b></font>';
+		fileData[i].aslr = '<font color="red"><b>NO</b></font>';
+
+		// TODO actually check
+	}
+}
+
+function handleFileSelect(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+
+	var files = evt.dataTransfer.files; // FileList
+	numFilesRead = 0;
+	filesToRead = files.length;
+	fileData = []
+
+	for (var i = 0; i < files.length; i++) {
+		(function(i)
+		{
+			//output.push(evaluateFile(files[0]));
+			fileData[i] = {};
+			fileData[i].name = files[i].name;
+			fileData[i].type = '';
+			reader = new FileReader();
+			fileData[i].reader = reader;
+			var fileNum = i;
+			reader.onloadend = function(evt) { handleFinishedRead(evt, fileNum); }
+			readFile(reader, files[i]);
+		})(i);
+	}
+	
+}
+
+function readFile(reader, file) {
+	end = MAX_FILE_SIZE;
+	var blob;
+	if(file.webkitSlice) {
+		blob = file.webkitSlice(0, end);
+	} else if(file.mozSlice) {
+		blob = file.mozSlice(0, end);
+	}
+
+	reader.readAsArrayBuffer(blob);
+}
+
+function handleDragOver(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+	evt.dataTransfer.dropEffect = 'copy';
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Main
+///////////////////////////////////////////////////////////////////////////////
+
+//Setup the dnd listeners.
+var dropZone = document.getElementById('container');
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileSelect, false);
